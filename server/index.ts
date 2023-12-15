0ba9989977c84c677025112fb6b0330edde3272a
Yura Ilyaev <yura.ilyaev@lifestreet.com>
2023-12-14 21:40:47 -0800

    chore: update logs

--- server/index.ts
+++ server/index.ts
@@ -14,15 +14,22 @@ const server = Bun.serve({
     port: process.env.PORT || 80,
     fetch(req: Request) {
         const IP: any = server.requestIP(req);
+        let ip = IP ? IP.address || "" : "";
+        ip = ip.replace("::ffff:", "");
         const url = new URL(req.url);
         let pathname = url.pathname;
         const domains = url.host.toLowerCase().split(".").slice(0, -1);
         const domain = domains[0] || "landing";
         const dir = DOMAIN_MAP[domain] || "landing";
 
+        const ts = new Date().toLocaleString();
+
         if (pathname === "/") pathname = "/index.html";
         if (pathname.indexOf("/api") === 0) {
-            return renderAPI(req, IP ? IP.address || "" : "");
+            return renderAPI(req, ip, ts);
+        }
+        if (pathname.indexOf("/index.html") > -1) {
+            console.log(ts + ": INDEX: ", pathname, " IP", ip);
         }
         const is_excluded = EXCLUSIONS.some((ex) => pathname.indexOf(ex) > -1);
         if (is_excluded) {
@@ -38,7 +45,7 @@ const server = Bun.serve({
     },
 });
 
-const renderAPI = async (req: Request, ip: string) => {
+const renderAPI = async (req: Request, ip: string, ts: string) => {
     const url = new URL(req.url);
     const its = url.pathname.split("/");
     const action = its[2] || "";
@@ -46,7 +53,7 @@ const renderAPI = async (req: Request, ip: string) => {
     const api = new ServerAPI(req, action, params);
     const result = await api.run();
     const res = new Response(JSON.stringify(result));
-    console.log("API CALL:", action, params, "IP", ip);
+    console.log(ts + ":", "API CALL:", action, params, "IP", ip);
     if (process.env.NODE_ENV === "development") {
         res.headers.set("Access-Control-Allow-Origin", "*");
         res.headers.set(
